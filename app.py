1431c6da5347205022749a8d1f7ef41ea50fc71d
Kiarash Golzadeh <kiarash.golzadeh@uwaterloo.ca>
2023-03-29 12:44:48 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import json
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route('/search/<retriever>')
+@cross_origin()
+def search(retriever):  # put application's code here
+    query = request.args.get("query", "")
+    print(retriever)
+    return retriever_result
+
+retriever_file = 'response.json'
+with open(retriever_file, 'r') as file:
+    retriever_result = json.load(file)
+if __name__ == '__main__':
+    app.run()
